2ab1330ec88279b4e07b9aaf65fe6bb64a6179b9
@mudassir2 <mudassir@nimapinfotech.com>
2024-01-10 16:27:45 +0530

    choose us, our pg, counter

--- main.js
+++ main.js
@@ -10,3 +10,41 @@ mobileMenu.addEventListener('click', () => {
 navMenu.addEventListener('click', () => {
   navMenu.classList.remove('show__menu')
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  function animateCounter(elementId, finalValue) {
+    let currentCount = 0
+    const counterElement = document.getElementById(elementId)
+
+    function updateCounter() {
+      counterElement.textContent = currentCount + '+'
+      currentCount++
+
+      if (currentCount <= finalValue) {
+        requestAnimationFrame(updateCounter)
+      }
+    }
+
+    updateCounter()
+  }
+
+  const section = document.getElementById('counterSection')
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  }
+
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        animateCounter('happyClients', 150)
+        animateCounter('projectsCompleted', 120)
+        animateCounter('teamMembers', 35)
+        observer.unobserve(entry.target)
+      }
+    })
+  }, options)
+
+  observer.observe(section)
+})
